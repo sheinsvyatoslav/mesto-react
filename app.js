f5eb86af7318109c5c6e3d01d858a81acbcef842
Svyatoslav <sheinsvyatoslav@gmail.com>
2022-07-03 21:46:07 +0300

    bugs fixed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const NotFoundError = require('./errors/not-found-error');
 const { vaidateSignup, vaidateSignin } = require('./middlewares/validation');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 5000 } = process.env;
 const app = express();
 app.use(cors());
 
